754b0b6df0b9efe640aa709e3c0f1c2d5a2796dc
Edoardo Filippucci <106374572+edoardofilippucci@users.noreply.github.com>
2023-12-28 00:59:42 +0100

    Add files via upload

---
+++ languages/javascript/custom-cursor.js
@@ -0,0 +1,29 @@
+// JavaScript per il tuo cursore personalizzato
+var cursor = document.querySelector(".cursor");
+var cursorinner = document.querySelector(".cursor2");
+var a = document.querySelectorAll("a");
+
+document.addEventListener("mousemove", function (e) {
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+  cursorinner.style.left = e.clientX + "px";
+  cursorinner.style.top = e.clientY + "px";
+});
+
+document.addEventListener("mousedown", function () {
+  cursor.classList.add("click");
+  cursorinner.classList.add("cursorinnerhover");
+});
+
+document.addEventListener("mouseup", function () {
+  cursor.classList.remove("click");
+  cursorinner.classList.remove("cursorinnerhover");
+});
+
+a.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    cursor.classList.add("hover");
+  });
+  item.addEventListener("mouseleave", () => {
+    cursor.classList.remove("hover");
+  });
+});
