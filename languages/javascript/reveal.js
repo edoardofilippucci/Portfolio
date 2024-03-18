ce9679efe2119390fb02bd4be23433d79dc72121
Edoardo Filippucci <106374572+edoardofilippucci@users.noreply.github.com>
2024-03-18 23:44:52 +0100

    Add files via upload

---
+++ languages/javascript/reveal.js
@@ -0,0 +1,35 @@
+ScrollReveal().reveal(".reveal-top", {
+  distance: "100px",
+  duration: 1500,
+  origin: "top",
+  easing: "cubic-bezier(.215, .61, .355, 1)",
+  interval: 600,
+  reset: "true",
+});
+
+ScrollReveal().reveal(".reveal-bottom", {
+  distance: "100px",
+  duration: 1500,
+  origin: "bottom",
+  easing: "cubic-bezier(.215, .61, .355, 1)",
+  interval: 600,
+  reset: "true",
+});
+
+ScrollReveal().reveal(".reveal-right", {
+  distance: "100px",
+  duration: 1500,
+  origin: "right",
+  easing: "cubic-bezier(.215, .61, .355, 1)",
+  interval: 600,
+  reset: "true",
+});
+
+ScrollReveal().reveal(".reveal-left", {
+  distance: "100px",
+  duration: 1500,
+  origin: "left",
+  easing: "cubic-bezier(.215, .61, .355, 1)",
+  interval: 600,
+  reset: "true",
+});
